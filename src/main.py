5ce99999802373f0f35669310bd2c127ed0654b3
Zain-Azi <144179855+Zain-Azi@users.noreply.github.com>
2025-10-25 15:34:27 +0100

    added Text Box functionality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 import pygame
 
 from window import Window
+from textbox import TextBox
 
-window = Window("Hacknotts pet", 800, 600)
+window = Window("Cheppie the Dragon", 800, 600)
+textbox = TextBox(50, 550, 700, 40)
+
 exit = False
 
 while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
+
+        result = textbox.handle_event(event)
+        if result is not None:
+            user_input = result
+            print(user_input)  
+
+    textbox.draw(window._Window__screen)   
     pygame.display.update()
