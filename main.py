a832b7b2d6c033e7c9698e19358e3bf81ffa2e80
Zain-Azi <144179855+Zain-Azi@users.noreply.github.com>
2025-10-25 14:43:42 +0100

    Added Window

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("aaa")
+import pygame
+
+pygame.init()
+
+canvas = pygame.display.set_mode((1280, 720))
+
+pygame.display.set_caption("HackNotts Pet")
+exit = False
+
+while not exit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+    pygame.display.update()
